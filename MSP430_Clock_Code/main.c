cd0206926fdd60bc60c3b106939040f374728b33
Chris Cannon <Chris Cannon>
2017-04-24 05:10:12 -0400

    Add loop

--- MSP430_Clock_Code/main.c
+++ MSP430_Clock_Code/main.c
@@ -9,4 +9,12 @@ void main(void) {
     P1DIR = 0x00;
     P2DIR = 0x00;
 
+    volatile int currentSeconds = 0;
+
+    while(1)
+    {
+        displayCurrentSeconds(currentSeconds);
+        currentSeconds++;
+        __delay_cycles(1000);
+    }
 }
