3c47b43fd09766870c4d9a137f5c4b0752394931
Chris Cannon <Chris Cannon>
2017-05-01 02:01:02 -0400

    Test seven-segment display

--- MSP430_Clock_Code/main.c
+++ MSP430_Clock_Code/main.c
@@ -6,14 +6,16 @@
 void main(void) {
     WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
 	
-    P1DIR = 0x00;
-    P2DIR = 0x00;
+    P1DIR = 0xFF;
+    P2DIR = 0xFF;
 
     volatile int currentSeconds = 0;
 
+    P1OUT = 0xFF;
+
     while(1)
     {
-        displayCurrentSeconds(currentSeconds);
+        //displayCurrentSeconds(currentSeconds);
         currentSeconds++;
         __delay_cycles(1000);
     }
