edbcd688806df998fd33ae348411e1dcb0e801ff
Chris Cannon <Chris Cannon>
2017-05-01 02:33:21 -0400

    End of work time

--- MSP430_Clock_Code/main.c
+++ MSP430_Clock_Code/main.c
@@ -1,22 +1,64 @@
 #include <msp430.h> 
 
+int currentSeconds;
+
 /*
  * main.c
  */
-void main(void) {
+void main(void)
+{
     WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
 	
     P1DIR = 0xFF;
     P2DIR = 0xFF;
 
-    volatile int currentSeconds = 0;
-
-    P1OUT = 0xFF;
+    currentSeconds = 0;
 
     while(1)
     {
-        //displayCurrentSeconds(currentSeconds);
+        displayCurrentSeconds();
         currentSeconds++;
         __delay_cycles(1000);
+    }
+}
+
+void displayCurrentSeconds()
+{
+    volatile int lsd = currentSeconds % 10;
+    volatile int msd = currentSeconds / 10;
+
+    switch(lsd)
+    {
+        case 0:
+            P1OUT = 0x3F;
+            break;
+        case 1:
+            P1OUT = 0x06;
+            break;
+        case 2:
+            P1OUT = 0x5B;
+            break;
+        case 3:
+            P1OUT = 0x4F;
+            break;
+        case 4:
+            P1OUT = 0x66;
+            break;
+        case 5:
+            P1OUT = 0x6D;
+            break;
+        case 6:
+            P1OUT = 0x7D;
+            break;
+        case 7:
+            P1OUT = 0x07;
+            break;
+        case 8:
+            P1OUT = 0x7F;
+            break;
+        case 9:
+            P1OUT = 0x6F;
+            break;
+        default: break;
     }
 }
